98fb6b008ca99eac6c2c8c58fc6ff548978ed470
sonnka <sonyakazanceva1331@gmail.com>
2023-11-08 02:39:30 +0200

    Initial commit

---
+++ src/main/java/com/VigiDrive/VigiDriveApplication.java
@@ -0,0 +1,13 @@
+package com.VigiDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigiDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigiDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VigiDrive/VigiDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VigiDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigiDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
