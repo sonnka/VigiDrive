0d2cfaa78bf40296635b222dfef986e135ab2a0f
sonnka <sonyakazanceva1331@gmail.com>
2023-12-21 17:40:58 +0200

    Fixed bugs

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -23,14 +23,13 @@ CREATE TABLE accesses
 
 CREATE TABLE users
 (
-    user_id     BIGINT AUTO_INCREMENT NOT NULL,
-    first_name  VARCHAR(255)          NULL,
-    last_name   VARCHAR(255)          NULL,
-    email       VARCHAR(255)          NULL,
-    password    VARCHAR(255)          NULL,
-    avatar      VARCHAR(255)          NULL,
-    `role`      VARCHAR(255)          NULL,
-    keycloak_id BINARY(16)            NULL,
+    user_id    BIGINT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(255)          NULL,
+    last_name  VARCHAR(255)          NULL,
+    email      VARCHAR(255)          NULL,
+    password   VARCHAR(255)          NULL,
+    avatar     VARCHAR(255)          NULL,
+    `role`     VARCHAR(255)          NULL,
     CONSTRAINT pk_users PRIMARY KEY (user_id)
 );
 
@@ -102,20 +101,6 @@ CREATE TABLE health
 
 ALTER TABLE health
     ADD CONSTRAINT FK_HEALTH_ON_DRIVER FOREIGN KEY (driver_id) REFERENCES drivers (user_id);
-
-
-CREATE TABLE recommendations
-(
-    recommendation_id BIGINT AUTO_INCREMENT NOT NULL,
-    title             VARCHAR(255)          NULL,
-    `description`     VARCHAR(255)          NULL,
-    driver_id         BIGINT                NULL,
-    CONSTRAINT pk_recommendations PRIMARY KEY (recommendation_id)
-);
-
-ALTER TABLE recommendations
-    ADD CONSTRAINT FK_RECOMMENDATIONS_ON_DRIVER FOREIGN KEY (driver_id) REFERENCES drivers (user_id);
-
 
 
 CREATE TABLE situations
